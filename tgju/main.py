ed3dae4e849feb3752064caf420a2f017f64736e
Asncodes-80 <asn80.asn@hotmail.com>
2021-10-29 22:19:34 +0330

    Add my project in gh

---
+++ tgju/main.py
@@ -0,0 +1,36 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+
+def get_data_as_collection(url=None):
+    """
+    This is main brain of crawl page.
+    I find api.accessban.com from network  inspect of browser debug.
+    It's simple but we would have pation to get data from this static url.
+    URL is not matter, but there are two things, at first we want get data of one month,
+    so we have this param in URL start={}&length=30, start is start number, like 0-30 one month,
+    30-60 two month, 60-90 and etc.
+    And second we need to render from that.
+    """
+    try:
+        data = []
+
+        for i in range(0, 90, 30):
+            response = requests.get(static_url.format(i))
+            data.append(json.loads(response.text))
+
+        data = data[0]["data"]
+
+        for day in range(len(data)):
+            for section in data[day]:
+                print(section)
+
+            print("\n end day -------- {}".format(3 * "\n"))
+
+    except Exception as err:
+        print(err)
+
+
+static_url = "https://api.accessban.com/v1/market/indicator/summary-table-data/geram18?lang=fa&order_dir=asc&draw=1&columns%5B0%5D%5Bdata%5D=0&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=1&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=2&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=3&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=4&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=5&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=6&columns%5B6%5D%5Bname%5D=&columns%5B6%5D%5Bsearchable%5D=true&columns%5B6%5D%5Borderable%5D=true&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B7%5D%5Bdata%5D=7&columns%5B7%5D%5Bname%5D=&columns%5B7%5D%5Bsearchable%5D=true&columns%5B7%5D%5Borderable%5D=true&columns%5B7%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B7%5D%5Bsearch%5D%5Bregex%5D=false&start={}&length=30&search=&order_col=&order_dir=&from=&to=&convert_to_ad=1&_=1635508309004"
+get_data_as_collection(url=static_url)
